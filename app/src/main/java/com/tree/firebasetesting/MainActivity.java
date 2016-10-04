1776d6ef7c9d677dfcd490b86c66f02c0b3293ca
talha629 <talha629@gmail.com>
2016-10-05 04:53:44 +0600

    comment

--- app/src/main/java/com/tree/firebasetesting/MainActivity.java
+++ app/src/main/java/com/tree/firebasetesting/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mConditionRef.setValue("Sunny");
+                //jhjh
             }
         });
 
